76be2b1444f3269731e13cd01e3e8a0793018cd5
Mateusz Jurzyna <jurzyna@gmail.com>
2018-11-12 10:56:01 +0100

    [FREYJA-4] feat: documentation page of button

--- src/app/pages/button/button.module.ts
+++ src/app/pages/button/button.module.ts
@@ -4,13 +4,19 @@ import { ButtonComponent } from './button.component';
 import { ContentComponent } from './content/content.component';
 import {SubNavModule} from '../../layout/sub-nav/sub-nav.module';
 import {ContentWrapModule} from '../../components/content-wrap/content-wrap.module';
+import {FjButtonModule} from '@libs/freyja/src/lib/button/button.module';
+import {HighlightModule} from 'ngx-highlightjs';
+import {TextHelper} from '../../shared/helpers/text.helper';
 
 @NgModule({
   imports: [
     CommonModule,
     SubNavModule,
-    ContentWrapModule
+    ContentWrapModule,
+    FjButtonModule,
+    HighlightModule.forRoot({ theme: 'arduino-light'})
   ],
+  providers: [TextHelper],
   declarations: [ButtonComponent, ContentComponent],
   exports: [ButtonComponent]
 })
--- src/app/pages/button/content/content.component.ts
+++ src/app/pages/button/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TextHelper} from '../../../shared/helpers/text.helper';
 
 @Component({
   selector: 'fj-demo-content',
@@ -6,8 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.scss']
 })
 export class ContentComponent implements OnInit {
+  public codeType = this.textHelper.dedent`
+    <fj-button>Primary</fj-button>
+    <fj-button buttonStyle="light">Light</fj-button>
+    <fj-button [ghost]="true">Disabled</fj-button>
+    <fj-button buttonStyle="danger">Destructive</fj-button>
+  `;
 
-  constructor() { }
+  public codeSize = this.textHelper.dedent`
+    <fj-button buttonSize="lrg">Big</fj-button>
+    <fj-button buttonSize="med">Medium</fj-button>
+    <fj-button buttonSize="sm">Small</fj-button>
+    <fj-button buttonSize="micro">Micro</fj-button>
+  `;
+
+  public codeShape = this.textHelper.dedent`
+    <fj-button>Radius</fj-button>
+    <fj-button buttonShape="square">Square</fj-button>
+    <fj-button buttonShape="rounded">Round</fj-button>
+  `;
+
+  public codeIcon = this.textHelper.dedent`
+    <fj-button buttonIcon="plus">Button</fj-button>
+  `;
+
+  public codeStyle = this.textHelper.dedent`
+    <fj-button [ghost]="true" buttonIcon="plus" iconFill="#1b51a0">Ghost</fj-button>
+  `;
+  constructor(public textHelper: TextHelper) { }
 
   ngOnInit() {
   }
