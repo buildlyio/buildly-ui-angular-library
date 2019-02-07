6981817dd4bd18449425ba1f848568ae356f86df
Aziz <aziz@humanitec.com>
2019-02-07 10:41:01 +0100

    Add card-item page to Freyja showcase

---
+++ src/app/pages/card-item/card-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fj-demo-card-item',
+  templateUrl: './card-item.component.html',
+  styleUrls: ['./card-item.component.scss']
+})
+export class CardItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
