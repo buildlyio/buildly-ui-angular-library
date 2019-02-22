d53bea2d06353ac20e0cac005062179535daf20d
Aziz <aziz@humanitec.com>
2019-02-22 09:58:25 +0100

    remove svg icon from showcase page

--- src/app/layout/main-nav/main-nav.component.ts
+++ src/app/layout/main-nav/main-nav.component.ts
@@ -52,10 +52,10 @@ export class MainNavComponent implements OnInit {
       path: '/card-item',
       label: 'Card-item'
     },
-    {
-      path: '/svg-icon',
-      label: 'SVG Icon'
-    },
+    // {
+    //   path: '/svg-icon',
+    //   label: 'SVG Icon'
+    // }, // not working properly yet, should be fixed
     {
       path: '/card',
       label: 'Card'
