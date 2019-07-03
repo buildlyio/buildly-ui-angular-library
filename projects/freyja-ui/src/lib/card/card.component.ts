baaf9da46097b961b3ada43efabd939135e500ef
Aziz <aziz@humanitec.com>
2019-07-03 15:07:40 +0200

    add multiple theming for card

--- projects/freyja-ui/src/lib/card/card.component.ts
+++ projects/freyja-ui/src/lib/card/card.component.ts
@@ -1,11 +1,24 @@
 import {Component, Input} from '@angular/core';
+import { BaseComponent } from '../shared/base.component';
 
 @Component({
   selector: 'fj-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent {
+export class CardComponent extends BaseComponent {
   @Input() noMargin: false;
-  constructor() { }
+  constructor() {
+    super();
+  }
+  /**
+   * Add css classes based on inputs like size, shape etc
+   */
+  public generateClassList() {
+    const baseCls = 'fj-card--';
+    const classes = {
+      [baseCls + this.theme]: this.theme,
+    };
+    return classes;
+  }
 }
